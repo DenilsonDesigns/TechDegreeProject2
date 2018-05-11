5d03ffaaf3e716e7920f917c87197cd7a2d61a3f
DenilsonDesigns <38902985+DenilsonDesigns@users.noreply.github.com>
2018-05-11 14:01:35 +1000

    Basically finished one bug.
    Button 6 has a bug that it wont highlight as active when clicked.

--- Project2/student-list.js
+++ Project2/student-list.js
@@ -1,41 +1,43 @@
 //storing in variable all the student details on page.
-const studentDeets = document.getElementsByClassName('student-item'); // previously 'student-item cf'
+const studentDeets = document.getElementsByClassName('student-item');
+//selecting button a tags
+let allButtons= document.getElementsByTagName('A'); 
+
+//loading buttons
+appendPageLinks(studentDeets);
+//showing first page of results
+showPage(0, studentDeets);
+//assigning first button as active
+allButtons[0].className=  "active"; 
+
+//listening for clicks on page buttons, target gets active status
+document.querySelector('.pagination ul').addEventListener('click', (e)=>{
+    for (let i=0; i< allButtons.length; i++){
+        allButtons[i].className= "";
+        };
+    if (e.target.tagName=== "A"){
+        let pageNum = (e.target.textContent) -1;
+        showPage(pageNum, studentDeets);
+        e.target.className= "active";
+    }
+});
 
 
-
-// This ​function ​builds ​a ​list ​of ​ten ​students ​and ​displays ​it ​on ​the ​page. ​The ​students ​displayed
-// depends ​on ​the ​page ​number ​passed ​to ​this ​function. ​The ​function ​should ​loop ​through ​all ​the
-// students ​in ​the ​list ​and ​determine ​if ​each ​student ​is ​on ​this ​page. ​It ​will ​show ​all ​the ​students ​on
-// this ​page ​and ​hide ​the ​rest. ​Here ​are ​some ​ideas ​for ​how ​this ​could ​work ​in ​code:
-
-
+//function to build list of 10 students per page
 function showPage(pageNumber, studentDetails) {
-    // first hide all students on the page
+    //hiding all students
     for(let i=0; i<studentDetails.length; i++ ){
         studentDetails[i].style.display= 'none';
         }
-       
+    //building list of 10 students based on page number
     let counter= pageNumber*10;
     for(counter; (counter)< ((pageNumber*10)+10); counter++){
-    // then loop through all students in our student list argument
-    // if student should be on this page number
-    // show the student
         studentDetails[counter].style.display= 'block';
         }
     }
-
-//attempt to get 20-30 to show up.
-
-
-
-
-    // This ​function ​creates ​all ​the ​page ​links ​based ​on ​a ​list ​of ​students. ​It ​will ​determine ​how ​many
-    // pages ​we ​need ​based ​on ​the ​list's ​length, ​create ​a ​list ​of ​links ​for ​each ​page, ​and ​append ​that
-    // list ​to ​the ​page. ​When ​each ​link ​is ​clicked, ​we'll ​use ​the ​showPage ​function ​to ​display ​the
-    // corresponding ​page, ​and ​mark ​the ​active ​link. ​For ​example, ​clicking ​the ​link ​to ​page ​2 ​will ​tell
-    // the ​showPage ​function ​to ​display ​students ​11 ​through ​20.
-
-let allButtons= document.getElementsByTagName('a');
+   
+ 
+  //appending links to bottom of page
 function appendPageLinks(studentList) {
     // determine how many pages for this student list
     const noOfButtons= Math.ceil(studentList.length/10);
@@ -48,29 +50,16 @@ function appendPageLinks(studentList) {
     const ul= document.createElement('ul');
     paginDiv.appendChild(ul);
 
-    // "for" every page
+    // based on no of buttons required, adding that number of buttons. 
     for(let i=1; i<= noOfButtons; i++){
-        //create li
         const li= document.createElement('li');
-        //append li to ul
         ul.appendChild(li);
-        //add <a> with page number as textContent
         const a= document.createElement('a');
         li.appendChild(a);
+        a.href= "#";
         a.innerText= i;
     }
-
-
-    // define what happens when you click a link (event listener)
-
-
-    allButtons.addEventListener('click', (e)=>{
-        e.target.className= "active";
-    });
-    // Use showPage to display the page for the link clicked
-    showPage(1, studentDeets);
-    // mark that link as "active"
-    
 }   
 
-appendPageLinks(studentDeets);
+
+
